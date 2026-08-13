a0c8c62d2cbc43f70d2cb459c3cbf676dceb2cef
antoniodemax <anthony.onyango@student.moringaschool.com>
2026-08-13 15:36:26 +0300

    done

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -98,7 +98,7 @@ export default function Navigation() {
               <img
                 src="/logo.jpg"
                 alt="Antony Peter"
-                className="h-15 lg:h-18 w-auto object-contain brightness-110 transition-opacity duration-200 hover:opacity-90"
+                className="h-14 w-auto object-contain brightness-110 transition-opacity duration-200 hover:opacity-90"
               />
             </a>
 
